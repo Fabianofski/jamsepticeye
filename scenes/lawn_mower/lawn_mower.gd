f295617967f6a2e54b8440d8491d80661966bc50
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-10-01 20:07:49 +0200

    feat: add drift to lawn mower

--- scenes/lawn_mower/lawn_mower.gd
+++ scenes/lawn_mower/lawn_mower.gd
@@ -1,7 +1,8 @@
 extends RigidBody3D
 
 @export var acceleration = 20.0
-@export var steering = 1.5
+@export var steering_speed = 1.5
+@export var drift_speed = 1.5
 @export var max_speed = 30.0
 
 @onready var mesh: MeshInstance3D = $MeshInstance3D
@@ -14,8 +15,20 @@ func _ready():
 	camera_rig.top_level = true
 	mesh.top_level = true
 
-func _physics_process(delta):
+func get_steering(delta): 
 	var input_dir = 0.0
+	if Input.is_action_pressed("left"):
+		input_dir = -1.0
+	elif Input.is_action_pressed("right"):
+		input_dir = 1.0
+
+	if Input.is_action_pressed("drift"): 
+		var speed_factor = abs(speed) / max_speed
+		input_dir *= drift_speed * speed_factor
+
+	direction += input_dir * steering_speed * delta * (speed / max_speed)
+
+func get_speed(delta): 
 	if Input.is_action_pressed("forward"):
 		speed -= acceleration * delta
 	elif Input.is_action_pressed("back"):
@@ -25,17 +38,7 @@ func _physics_process(delta):
 
 	speed = clamp(speed, -max_speed, max_speed)
 
-	if Input.is_action_pressed("left"):
-		input_dir = -1.0
-	elif Input.is_action_pressed("right"):
-		input_dir = 1.0
-
-	direction += input_dir * steering * delta * (speed / max_speed)
-	var forward = Vector3(sin(direction), 0, cos(direction))
-	linear_velocity = forward * speed
-
-	mesh.position = position 
-	mesh.rotation.y = direction
+func update_camera(): 
 	camera_rig.global_transform.origin = lerp(
 		camera_rig.global_transform.origin, 
 		global_transform.origin, 0.1
@@ -44,3 +47,17 @@ func _physics_process(delta):
 		Basis(Quaternion(Vector3.UP, direction)),
 		0.1
 	)
+
+func update_mesh(): 
+	mesh.position = position 
+	mesh.rotation.y = direction
+
+func _physics_process(delta):
+	get_speed(delta)
+	get_steering(delta)
+
+	var forward = Vector3(sin(direction), 0, cos(direction))
+	linear_velocity = forward * speed
+
+	update_camera()
+	update_mesh()
