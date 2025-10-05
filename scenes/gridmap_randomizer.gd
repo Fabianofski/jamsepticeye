d78fd6935ce44804d57bfea5e5c7b8d310f1e488
Santum <39064333+santumerino@users.noreply.github.com>
2025-10-05 18:07:39 -0300

    add: House variants!!!

--- scenes/gridmap_randomizer.gd
+++ scenes/gridmap_randomizer.gd
@@ -10,11 +10,11 @@ func _run():
 	if gridmap == null:
 		return
 
-	var house_indices = [1,2]
+	var house_indices = [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
 	var used_cells = gridmap.get_used_cells()
 	for cell in used_cells:
 		var idx = gridmap.get_cell_item(cell)
 		if idx in house_indices:
 			var new_idx = house_indices[randi() % house_indices.size()]
-			gridmap.set_cell_item(cell, new_idx)
+			gridmap.set_cell_item(cell, new_idx, gridmap.get_cell_item_orientation(cell))
