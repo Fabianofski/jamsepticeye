f0e2fcd89e9e417be4ad4773a5693db7ac7e44b0
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-10-05 10:39:35 +0200

    fix: hats

--- scenes/collectible/zombie/zombie_randomizer.gd
+++ scenes/collectible/zombie/zombie_randomizer.gd
@@ -22,8 +22,6 @@ enum Personality { VICIOUS, SCAREDYCAT }
 @export_group("Hats")
 @export var hats: Array[Hat] = []
 @export var hat_chance = 0.3
-@onready var bone_attachment: BoneAttachment3D = $"../BoneAttachment3D"
-@onready var skeleton_path: NodePath = ^"../Mesh/Armature/Skeleton3D"
 
 @export_group("Behaviour")
 @export var personality: Personality = Personality.VICIOUS
@@ -66,14 +64,9 @@ func randomize_hat():
 			pool = hats.filter(func(h): return h.rarity == Hat.Rarity.EPIC)
 			collectible.money_amount *= epic_multiplier
 
-		bone_attachment.set_external_skeleton(skeleton_path)
-		bone_attachment.bone_name = "neck"
-		bone_attachment.bone_idx = 4
-
 		var hat = pool[randi() % pool.size()]
 		collectible.rarity = hat.rarity
 		hat.set_visible(true)
-		hat.scale = mesh.scale
 
 func randomize_personality():
 	var enum_values = Personality.values()
